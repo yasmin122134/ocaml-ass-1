d556e803a3baec1c6ea57957e530acbcd8ed99d6
yasmin12213 <yasmin122134@gmail.com>
2024-12-04 22:37:48 +0200

    added 2

---
+++ comparator_tree.ml
@@ -0,0 +1,20 @@
+(* Define the binary tree type *)
+type 'a binary_tree = 
+  | Empty
+  | Node of 'a * 'a binary_tree * 'a binary_tree;;
+
+(* Define the comparator_tree function *)
+let rec comparator_tree tree elem comparator =
+  match tree with
+  | Empty -> Node (elem, Empty, Empty)
+  | Node (value, left, right) ->
+      if comparator elem value <= 0 then
+        Node (value, comparator_tree left elem comparator, right)
+      else
+        Node (value, left, comparator_tree right elem comparator);;
+
+(* Define an integer comparator function *)
+let int_comparator x y = 
+  if x < y then -1 
+  else if x > y then 1 
+  else 0;;
