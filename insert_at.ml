f7d17265987f1ab107f536c0ee6f88161f754a37
yasmin12213 <yasmin122134@gmail.com>
2024-12-04 15:31:11 +0200

    now passing tests

--- insert_at.ml
+++ insert_at.ml
@@ -1,7 +1,5 @@
-let rec insert_at index member lst = 
-    match lst, index with
-    | [], 0 -> [member]
-    | [], _ when index > 0 -> lst
-    | hd :: tl, 0 -> member :: lst
-    | hd :: tl, _ when index > 0 -> hd :: insert_at (index - 1) member tl
-    | lst, _ -> lst
+let rec insert_at member index lst =
+  match index, lst with
+  | 0, _ -> member :: lst
+  | _, [] -> lst
+  | i, hd :: tl -> hd :: insert_at member (i - 1) tl
--- insert_none_at.ml
+++ insert_none_at.ml
@@ -1,5 +1,10 @@
-(* insert_none_at.ml *)
+let rec insert_at member index lst =
+  match index, lst with
+  | 0, _ -> member :: lst
+  | _, [] -> lst
+  | i, hd :: tl -> hd :: insert_at member (i - 1) tl
+
+
 
 let insert_none_at index lst =
-  Insert_at.insert_at index None lst
-
+  insert_at "None" index lst
